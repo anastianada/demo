2667a62cf16c59753c3a47f5472d36e801fa050a
Daniel Christianto <daniel.christianto3@gmail.com>
2022-04-26 22:56:05 +0700

    refactor demo application

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -13,8 +13,6 @@ import java.util.List;
 
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args); }
 
 }
