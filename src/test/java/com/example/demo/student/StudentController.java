c2fba761fe0a8b6a1522d0039a7f0b29b9533f21
anastianada <anastia1504@gmail.com>
2022-04-28 10:35:02 +0700

    memperbaharui github

--- src/test/java/com/example/demo/student/StudentController.java
+++ src/test/java/com/example/demo/student/StudentController.java
@@ -5,12 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/student")
+@RequestMapping(path="api/v1/student")
 public class StudentController {
 
     private final StudentService studentService;
@@ -22,6 +20,6 @@ public class StudentController {
 
     @GetMapping
     public List<Student> getStudents() {
-        return studentService.getStudents();
+        return studentService.getStudents ();
     }
 }
--- src/test/java/com/example/demo/student/StudentService.java
+++ src/test/java/com/example/demo/student/StudentService.java
@@ -1,8 +1,6 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -13,11 +11,11 @@ public class StudentService {
     public List<Student> getStudents() {
         return List.of(
                 new Student(
-                        id:1L,
-                        name:"Maria",
-                        email:"mariam.jamal@gmail.com",
-                        LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5),
-                        age:21
+                        1L,
+                        "Maria",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
 				)
 		);
     }
